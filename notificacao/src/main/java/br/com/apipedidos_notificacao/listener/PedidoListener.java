d38d5714b60ab1401c38eb746c8613da70ed00c7
ElderFavsto <elderfavsto@gmail.com>
2025-09-15 17:00:35 -0300

    feat: adiciona metodos do EmailService e PedidoListener

--- notificacao/src/main/java/br/com/apipedidos_notificacao/listener/PedidoListener.java
+++ notificacao/src/main/java/br/com/apipedidos_notificacao/listener/PedidoListener.java
@@ -1,17 +1,27 @@
 package br.com.apipedidos_notificacao.listener;
 
 import br.com.apipedidos_notificacao.entities.Pedido;
+import br.com.apipedidos_notificacao.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PedidoListener {
 	private final Logger logger = LoggerFactory.getLogger(PedidoListener.class);
 	
+	
+	private final EmailService emailService;
+	
+	public PedidoListener(EmailService emailService) {
+		this.emailService = emailService;
+	}
+	
 	@RabbitListener(queues = "pedidos.v1.pedido-criado.gerar.notificacao")
 	public void enviarNotificacao(Pedido pedido) {
+		emailService.enviarEmail(pedido);
 		logger.info("Noficacao gerada: {}", pedido.toString());
 	}
 }
--- notificacao/src/main/java/br/com/apipedidos_notificacao/service/EmailService.java
+++ notificacao/src/main/java/br/com/apipedidos_notificacao/service/EmailService.java
@@ -1,4 +1,35 @@
 package br.com.apipedidos_notificacao.service;
 
+import br.com.apipedidos_notificacao.entities.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	@Autowired
+	private MailSender mailSender;
+	
+	public void enviarEmail(Pedido pedido) {
+		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+		simpleMailMessage.setFrom("jimpo-do-danone@gmail.com");
+		simpleMailMessage.setTo(pedido.getEmailNotificacao());
+		simpleMailMessage.setSubject("Pedido de compra");
+		simpleMailMessage.setText(this.gerarMensagem(pedido));
+		mailSender.send(simpleMailMessage);
+	}
+	
+	private String gerarMensagem(Pedido pedido) {
+		String pedidoId = pedido.getId().toString();
+		String cliente = pedido.getCliente();
+		String valor = String.valueOf(pedido.getValorTotal());
+		String status = pedido.getStatus().name();
+		return String.format("Olá %s seu pedido de numero %s no valor de %s, foi realizado com sucesso \nStatus: %s.",
+						cliente, pedidoId, valor, status);
+	}
 }
